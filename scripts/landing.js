0d73028cf8e69d91feac83641e179f0096283a66
Michelle Mross <mross@Michelles-MacBook-Pro.local>
2017-07-06 22:48:02 -0400

    assignment-16-complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,25 @@
-
  var animatePoints = function() {  
  var revealPoint = function() {
-       
-       $(this).css({
-           opacity: 1, 
-           transform: 'scaleX(1) translateY(0)'
-           
-       });
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
    };
-       $.each($('.point'), revealPoint);
+     $.each($('.point'), revealPoint);
        
-$window.load(function) {
+ $(window).load(function() {
     if ($(window).height() > 950) {
-        animatePoints();
-    } 
-    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-    $(window).scroll(function(event) {
+         animatePoints();
+     }
+     // #2
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     // #3
+     $(window).scroll(function(event) {
+         // #4
          if ($(window).scrollTop() >= scrollDistance) {
              animatePoints();
-        }
-    });
-         
+         }
+     });
+ });  
          
